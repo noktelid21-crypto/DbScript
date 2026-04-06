5412147240f9a080e8308e6a699ef57a8e1bab4f
noktelid21-crypto <noktelid21@gmail.com>
2026-04-06 16:40:13 +0700

    Update token list

--- Token.js
+++ Token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8237599669:AAHRuBA2r_7bubSlc2nQ4mruas_G1FBpQRQ"
+    "8237599669:AAHRuBA2r_7bubSlc2nQ4mruas_G1FBpQRQ",
+    "8750651439:AAE485BGf7nmG_TwX_RgaBmSOv1CzJRbevQ"
   ]
 }
